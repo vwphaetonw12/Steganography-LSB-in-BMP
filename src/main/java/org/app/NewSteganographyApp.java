d1d0d873a87d0208bfedc400ef3a6e58ac410094
vwphaetonw12 <mama.dr352@gmail.com>
2024-12-13 22:34:52 +0300

    last
    last

---
+++ src/main/java/org/app/NewSteganographyApp.java
@@ -0,0 +1,9 @@
+package org.app;
+
+import javafx.application.Application;
+
+public class NewSteganographyApp {
+    public static void main(String[] args) {
+        Application.launch(SteganographyApp.class, args);
+    }
+}
