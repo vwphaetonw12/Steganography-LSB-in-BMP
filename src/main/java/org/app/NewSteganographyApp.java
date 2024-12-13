d274a33f462a33d580db44bf1661fc57fe81eda7
vwphaetonw12 <mama.dr352@gmail.com>
2024-12-13 22:31:31 +0300

    456456
    456456

--- src/main/java/org/app/NewSteganographyApp.java
+++
@@ -1,9 +0,0 @@
-package org.app;
-
-import javafx.application.Application;
-
-public class NewSteganographyApp {
-    public static void main(String[] args) {
-        Application.launch(SteganographyApp.class, args);
-    }
-}
